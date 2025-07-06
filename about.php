dedaf129ef12d3c0ffe8978e9277865a4a0418b4
Kiratipat sawangsisombat <capacity1412@gmail.com>
2025-07-06 19:55:53 +0700

    git lab ep 3

---
+++ about.php
@@ -0,0 +1,31 @@
+<?php
+$name = "Kiratipat sawangsisombat";
+$university = "Mahanakorn University of Technology";
+$faculty = "Computer Engineering and AI";
+$department = "Computer Engineering";
+$studentId = "6711860001";
+$year = "2";
+$semester = "1";
+$course = "MIIC1324";
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About/title>
+</head>
+
+<body>
+    <h1><?php echo $name; ?></h1>
+    <p>University: <?php echo $university; ?></p>
+    <p>Faculty: <?php echo $faculty; ?></p>
+    <p>Department: <?php echo $department; ?></p>
+    <p>Student ID: <?php echo $studentId; ?></p>
+    <p>Year: <?php echo $year; ?></p>
+    <p>Semester: <?php echo $semester; ?></p>
+    <p>Course: <?php echo $course; ?></p>
+</body>
+
+</html>
